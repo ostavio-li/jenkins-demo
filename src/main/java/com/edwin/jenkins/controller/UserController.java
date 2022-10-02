c72b44af961cd19aca5798611c7164033059301d
ostavio <ostavio@163.com>
2022-10-02 21:51:43 +0800

    初始提交

---
+++ src/main/java/com/edwin/jenkins/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.edwin.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "This is user list";
+    }
+
+}
